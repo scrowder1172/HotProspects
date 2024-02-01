ec0da172c68f68f5ed127ff8625e101ec14cc7fa
Scott Crowder <scrowder@gmail.com>
2024-02-01 09:28:35 -0600

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by SCOTT CROWDER on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
