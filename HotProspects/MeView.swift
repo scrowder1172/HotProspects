4a850a6944ae391145bdd80bd34c8320be4eac6c
Scott Crowder <scrowder@gmail.com>
2024-02-05 17:54:49 -0600

    generate personalized QR code

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -6,10 +6,47 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct MeView: View {
+    
+    @State private var name: String = "Anonymous"
+    @State private var emailAddress: String = "you@yoursite.com"
+    
+    let context: CIContext = CIContext()
+    let filter: CIQRCodeGenerator = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text("Me")
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                
+                TextField("Email", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            .navigationTitle("Your QR Code")
+        }
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
