5b1ca61d5a2d9b9e14961c87c077f34a78ed3371
Scott Crowder <scrowder@gmail.com>
2024-02-05 16:51:30 -0600

    show prospects

---
+++ HotProspects/MeView.swift
@@ -0,0 +1,18 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by SCOTT CROWDER on 2/5/24.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeView()
+}
---
+++ HotProspects/Prosepct.swift
@@ -0,0 +1,8 @@
+//
+//  Prosepct.swift
+//  HotProspects
+//
+//  Created by SCOTT CROWDER on 2/5/24.
+//
+
+import Foundation
