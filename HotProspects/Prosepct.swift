acd3b0b4b5dff5c9a30f0268ccdee9c1b0825afb
Scott Crowder <scrowder@gmail.com>
2024-02-05 16:51:53 -0600

    Show prospects

--- HotProspects/Prosepct.swift
+++ HotProspects/Prosepct.swift
@@ -5,4 +5,17 @@
 //  Created by SCOTT CROWDER on 2/5/24.
 //
 
-import Foundation
+import SwiftData
+
+@Model
+final class Prospect {
+    var name: String
+    var emailAddress: String
+    var isContacted: Bool
+    
+    init(name: String, emailAddress: String, isContacted: Bool) {
+        self.name = name
+        self.emailAddress = emailAddress
+        self.isContacted = isContacted
+    }
+}
--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -6,13 +6,65 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ProspectsView: View {
+    
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: \Prospect.name) var prospects: [Prospect]
+    
+    let filter: FilterType
+    
+    var title: String {
+        switch filter {
+        case .none:
+            "Everyone"
+        case .contacted:
+            "Contacted People"
+        case .uncontacted:
+            "Uncontacted People"
+        }
+    }
+    
+    init(filter: FilterType) {
+        self.filter = filter
+        
+        if filter != .none {
+            let showContactedOnly: Bool = filter == .contacted
+            
+            _prospects = Query(filter: #Predicate{
+                $0.isContacted == showContactedOnly
+            }, sort: [SortDescriptor(\Prospect.name)])
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(prospects) { prospect in
+                VStack(alignment: .leading) {
+                    Text(prospect.name)
+                        .font(.headline)
+                    
+                    Text(prospect.emailAddress)
+                        .foregroundStyle(.secondary)
+                }
+            }
+            .navigationTitle(title)
+            .toolbar {
+                Button("Add Prospect", systemImage: "qrcode.viewfinder") {
+                    let prospect: Prospect = Prospect(name: "Paul Hudson", emailAddress: "paul@hackingwithswift.com", isContacted: false)
+                    modelContext.insert(prospect)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    ProspectsView()
+    ProspectsView(filter: .none)
+        .modelContainer(for: Prospect.self)
 }
